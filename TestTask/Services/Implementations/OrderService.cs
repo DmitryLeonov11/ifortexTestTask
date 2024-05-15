92145980a2c51e1bfa1b62756604da418f6e4757
DmitriyLeonov <dleonov701@gmail.com>
2024-05-15 11:16:59 +0300

    Add project files.

---
+++ TestTask/Services/Implementations/OrderService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Enums;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class OrderService : IOrderService
+    {
+        private readonly ApplicationDbContext _db;
+
+        public OrderService(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public async Task<Order> GetOrder()
+        {
+            return await _db.Orders.Where(o => o.Quantity > 1).OrderByDescending(o => o.CreatedAt).FirstAsync();
+        }
+
+        public async Task<List<Order>> GetOrders()
+        {
+            return await _db.Orders.Where(u => u.User.Status == UserStatus.Active)
+                .OrderByDescending(o => o.CreatedAt).ToListAsync();
+        }
+    }
+}
